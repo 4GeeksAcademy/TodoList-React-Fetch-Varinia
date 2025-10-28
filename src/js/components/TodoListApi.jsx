16349cbab5ed861eeaeb7b8f5b87f11a2b820c84
Varinia <v.mesias.r@gmail.com>
2025-10-28 03:55:30 +0000

    falta agregar tarea en const y en input

--- src/js/components/TodoListApi.jsx
+++ src/js/components/TodoListApi.jsx
@@ -9,7 +9,7 @@ const TodoListApi = () => {
 		fetch (API_URL+"users/varinia",{
             method: "POST",
             headers: {
-                "Content-Type":"aplication"/json
+                "Content-Type":"aplication/json"
             },
         })
 		.then(response => response.json())
@@ -17,7 +17,7 @@ const TodoListApi = () => {
 		.catch (error => { console.log(error)});
 	}
 
-const traerTareas = () => {
+const traerLista = () => {
 	const API_URL = 'https://playground.4geeks.com/todo/';
 	fetch (API_URL+"users/varinia")
 	.then((response) => {
@@ -26,27 +26,57 @@ const traerTareas = () => {
         }    
         return response.json()
     })
-	.then ((data) =>setLista(data.todos))
+	.then ((data) =>setLista(data.todos || []))
 	.catch (error => { console.log(error)});
 }
 
+const eliminarTarea = (id) =>{
+    fetch (`https://playground.4geeks.com/todo/todos/${id}`,{
+            method: "DELETE"
+    })
+    .then ((response)=>{
+        if (response.ok){
+            console.log('Tarea con id ${id} eliminada');
+            traerLista();
+        }else{
+            console.log("No se elimino la tarea");
+        }        
+    })
+.catch((error)=>console.log(error));
+}
+const eliminarLista = () => {
+		const API_URL = 'https://playground.4geeks.com/todo/';
+		fetch (API_URL+"users/varinia",{
+            method: "DELETE",
+        })
+		.then(()=>{
+        console.log("Eliminaste todas las tareas");
+		setLista([]);
+        crearUsuario();
+    })
+		.catch (error => { console.log(error)});
+	}
+
 useEffect(()=>{
-    traerTareas()
+    traerLista()
 },[])
 
 
 	return (
-		<div className="paper bg-body text-body">
-			<h1>Todo List Api-Fetch</h1>
+		<div className="paper bg-body text-body p-3 mb-5 rounded border-bottom">
+			<h1 className="text-center">Todo List Api-Fetch</h1>
             <input type="text" placeholder="What needs to be done?"/>
             <ul className="list-unstyled text-start ">
                 {lista.map((item)=>(
                 <li key={item.id}>
-                    {item.label} <span onClick={()=>eliminarTarea(index)}> χ</span>
+                    {item.label} <span onClick={()=>eliminarTarea(item.id)}> χ</span>
                 </li>
             ))}
             </ul>
-            <p>{lista.length} item left</p>  
+            <div className="d-flex justify-content-between">
+                <p>{lista.length} item left</p>
+                <button className="btn btn-info">Eliminar todas las tareas</button>
+            </div>
 		</div>
 	);
 };
